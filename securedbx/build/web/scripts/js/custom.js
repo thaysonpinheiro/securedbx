92fde03f1483f2cacba138b89a27b19bd40fa2d8
Italo Sousa <>
2016-07-08 11:23:50 -0300

    Código do velocimetro

--- securedbx/build/web/scripts/js/custom.js
+++ securedbx/build/web/scripts/js/custom.js
@@ -5,21 +5,35 @@ function showRow(rowId){
 
 $( document ).ready(function() {
     $('#btn_connection').click(function(){
-        $.post("ServletSgbd", {host: $('#host').val(),
-                              port: $('#port').val(),
-                              base: $("#base").val(),
-                              user: $("#user").val(),
-                              password: $('#password').val(),
-                              sgbd: $('#sgbd').val()}, function( data ){ 
-                              
-                                    if(data!=0){
-                                        $('#result').html("FAIL");
-                                        $('#result').removeClass("alert-success");
-                                        $('#result').addClass("alert-danger");              
+        if($('#host').val()!="" && $('#port').val()!="" && $('#base').val()!="" && $('#user').val()!="" && $('#password').val()!="" && $('#sgbd').val()!=""){
+            $.post("ServletSgbd", { host: $('#host').val(),
+                                    port: $('#port').val(),
+                                    base: $("#base").val(),
+                                    user: $("#user").val(),
+                                    password: $('#password').val(),
+                                    sgbd: $('#sgbd').val()}, function( data ){ 
+
+                                    if(data==0){
+                                        alert("Invalid informations!");
+
                                     }else{
-                                        window.location.assign(data);  
+                                        
+                                        $.cookie('host', $('#host').val(), {expires: 1});
+                                        $.cookie('port', $('#port').val(), {expires: 1});
+                                        $.cookie('base', $('#base').val(), {expires: 1});
+                                        $.cookie('user', $('#user').val(), {expires: 1});
+                                        $.cookie('password', $('#password').val(), {expires: 1});
+                                        $.cookie('sgbd', $('#sgbd').val(), {expires: 1});
+                                        
+                                        //confirm(data[1].p4);
+                                        window.location.assign(data);
+                                       
                                     }
-                              });
+            });
+
+        }else{
+            alert("Enter all fields!");
+        }
     });
 
     $('#btn_cancel').click(function(){
@@ -33,4 +47,28 @@ $( document ).ready(function() {
         $('#password').val("");
         $('#sgbd').val("oracle");
     });
+    
+    function checkParameters(fields){
+
+    }
+    
+    function onlyNumber(fields){
+        $(fields).unbind('keyup').bind('keyup',function(e){
+            var thisVal = $(this).val();
+            var tempVal = "";
+
+            for(var i = 0; i<thisVal.length; i++){
+                    if(RegExp(/^[0-9]$/).test(thisVal.charAt(i))){
+                            tempVal += thisVal.charAt(i); 
+
+                            if(e.keyCode == 8){
+                                    tempVal = thisVal.substr(0,i); 
+                            }						
+                    }
+            }			
+            $(this).val(tempVal);
+        });
+    }
+
+    onlyNumber($('input[id="port"]'));
 });
